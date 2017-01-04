ac8b2c759e1c8aedd2c9e941e0f9076565726d3a
Joker <xiejian_hao@163.com>
2017-01-04 11:03:56 +0800

    提交Test

---
+++ src/xie2333/Test.java
@@ -0,0 +1,14 @@
+package xie2333;
+
+import java.io.Serializable;
+
+public class Test implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public final String name="混世魔王";
+	
+
+}
