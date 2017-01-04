9e287f65f69cb523d5d274aac3ca54a9fe2339fe
Joker <xiejian_hao@163.com>
2017-01-04 10:52:11 +0800

    提交时间

--- src/xie2333/Demo.java
+++ src/xie2333/Demo.java
@@ -1,8 +1,12 @@
 package xie2333;
 
+import java.util.Date;
+
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("11111");
 		System.out.println("xiejh");
+		Date date=new Date();
+		date.getTime();
 	}
 }
