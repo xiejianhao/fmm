68d201bc24b2feb18a1da1b961deebde0d1f66c4
Joker <xiejian_hao@163.com>
2017-01-05 16:54:39 +0800

    删除测试

--- src/xie2333/Test.java
+++ src/xie2333/Test.java
@@ -9,6 +9,8 @@ public class Test implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 	public final String name="混世魔王";
-	
+	public static void main(String[] args) {
+		System.out.println("1111111111");
+	}
 
 }
